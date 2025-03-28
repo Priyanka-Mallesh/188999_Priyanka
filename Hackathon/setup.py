d275ebe89ec5811bf57f5879dbc0a84aff173e77
Priyanka-Mallesh <priyamallesh2000@gmail.com>
2025-03-28 17:40:00 +0530

    Add files via upload

---
+++ Hackathon/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='subpackage',
+    version='0.1',
+    description='Testing installation of Primes Package',
+    url='#',
+    author='priyanka',
+    author_email='sample@gmail.com',
+    license='GPL',
+    packages=['subpackage'],
+    zip_safe=False
+    )
